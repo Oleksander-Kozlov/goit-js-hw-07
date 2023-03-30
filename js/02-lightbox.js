0bb2269f25826e28fe8e8066b1d583c5e150dbb1
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2023-03-31 00:42:34 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,7 +6,7 @@ list.classList.add("js-gallery");
 const cards = galleryItems.map(
   ({ preview, original, description }) => `<li class="gallery__item">
    <a class="gallery__link" href="${original}">
-      <img class="gallery__image" src="${preview}" alt="${description}"  />
+      <img class="gallery__image" src="${preview}" alt="${description}" title = "${description}"/>
    </a>
 </li>`
 );
