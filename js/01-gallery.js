b14e878fe303bc2b5a9a91c334db8877ca94d9d0
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2023-03-30 19:36:20 +0300

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector(".gallery");
+list.classList.add("js-gallery");
+const cards = galleryItems.map(
+  ({ preview, original, description }) => `<li class="gallery__item">
+ <a class="gallery__link" href="${original}">
+     <img
+       class="gallery__image"
+       src="${preview}"
+       data-source="${original}"
+       alt="${description}"/>
+   </a>
+ </li>
+`
+);
+list.insertAdjacentHTML("beforeend", cards.join(""));
+
+list.addEventListener("click", onClick);
+function onClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.dataset.source) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="1200" height="600">
+`);
+
+    instance.show();
+ if (instance.visible()) {
+     document.addEventListener("keydown", closer)
+     function closer(event) {
+         if (event.code === "Escape") {
+          instance.close();  
+        }
+        };
+ }   
+}
+
+
+   
